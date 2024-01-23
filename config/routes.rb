f835bb7b7dfa013c16b10adc8fd53fa59a6b61b4
bhushaboinaanus <30932733-bhushaboinaanus@users.noreply.replit.com>
2024-01-23 06:41:26 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :articles # , only: [:show]
+  get 'pages/home'
+  get 'about', to: 'pages#about'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
